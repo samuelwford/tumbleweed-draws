82bb7fa6e3449c20b082c9fe8d7a1d53b5185220
Samuel Ford <samuel.ford@icloud.com>
2019-01-25 20:20:02 -0500

    show the admin menu only on the backdoor site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= session[:user_id] if session[:user_id]
   end
 
+  def in_the_backdoor?
+    request.host_with_port.include? "herokuapp.com"
+  end
+
+  helper_method :in_the_backdoor?
   helper_method :logged_in?
 end
